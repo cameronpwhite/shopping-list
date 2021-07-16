46701746361c4d3c1e272aaf7376ea24d8f72f61
Cameron White <cameronpwhite98@gmail.com>
2021-07-16 11:30:48 -0500

    first commit

---
+++ shopping.js
@@ -0,0 +1,19 @@
+const shoppingList = [
+    {
+        id: 1,
+        name: "milk",
+        isEdible: true
+    },
+    {
+        id: 2,
+        name: "cereal",
+        isEdible: true
+    },
+    {
+        id: 3,
+        name: "bleach",
+        isEdible: false
+    },
+]
+
+console.log(shoppingList);
