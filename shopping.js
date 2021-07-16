b79f3657b9c0756a38aa226e5296e1dc88d54984
Cameron White <cameronpwhite98@gmail.com>
2021-07-16 14:27:47 -0500

    Made it to assign unique ID to new Items

--- shopping.js
+++ shopping.js
@@ -16,4 +16,22 @@ const shoppingList = [
     },
 ]
 
+
+let addToShoppingList = (name) => {
+    let newItem = {};
+    //Assigning id to new item
+    const lastIndex = shoppingList.length - 1;
+    const currentLastItem = shoppingList[lastIndex];
+    const maxId = currentLastItem.id;
+    const idForNewItem = maxId + 1;
+    newItem.id = idForNewItem;
+    newItem.name = name;
+    newItem.isEdible = true;
+    newItem.dateCreated = "7-16-2021"
+    shoppingList.push(newItem)
+}
+
+addToShoppingList("grapes");
+addToShoppingList("bread");
+
 console.log(shoppingList);
